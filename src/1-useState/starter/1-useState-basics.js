de945aa1e56c95452a3520c574b0fa80f5b98f9d
SriLalitha2004 <164881628+SriLalitha2004@users.noreply.github.com>
2025-01-13 09:27:30 +0530

    Update 1-useState-basics.js

--- src/1-useState/starter/1-useState-basics.js
+++ src/1-useState/starter/1-useState-basics.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Index = () => {
+  const [count,setCount]=useState(0) 
+  onIn=prev=>{
+    return prev+1
+  }
+  onDe=prev=>{
+    return prev-1
+  }
+  
   return (
     <div>
-      <h1>helo index</h1>
+      <button>-</button>
+      <p>{count}</p>
+      <button>+</p>
     </div>
   );
 };
